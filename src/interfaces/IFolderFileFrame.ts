58b40e1f148431bebf9ccef6e45eb16b2bda4e94
Erin Rivas <erin@atomicbear.com>
2022-09-16 20:45:55 -0500

    more interfaces

---
+++ src/interfaces/IFolderFileFrame.ts
@@ -0,0 +1,5 @@
+export default interface IFolderFileFrame {
+  whoAmI: string,
+  dirChildren: string[],
+  fileChildren: string[]
+}
--- src/interfaces/classes/IFileIo.ts
+++ src/interfaces/classes/IFileIo.ts
@@ -3,6 +3,7 @@ import { Miners } from '@/protos/storage/types/storage/miners'
 import { Api } from '@/protos/storage/rest'
 import IFileHandler from '@/interfaces/classes/IFileHandler'
 import IWalletHandler from '@/interfaces/classes/IWalletHandler'
+import IFolderDownload from '@/interfaces/IFolderDownload'
 
 export default interface IFileIo {
 
@@ -18,5 +19,5 @@ export default interface IFileIo {
   forceProvider (toSet: Miners): void
 
   uploadFiles (toUpload: IFileHandler[], wallet: IWalletHandler): Promise<void>
-  downloadFile (fileAddress: string, wallet: IWalletHandler): Promise<IFileHandler>
+  downloadFile (fileAddress: string, wallet: IWalletHandler, isFolder: boolean): Promise<IFileHandler | IFolderDownload>
 }
