21bb3290cd6723ddabcd50c9a7e890f8696dffd9
Erin Rivas <erin@atomicbear.com>
2022-09-19 14:30:15 -0500

    adding class interfaces to export

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-import FileIo from './classes/fileIo'
-import FileHandler from './classes/fileHandler'
-import WalletHandler from './classes/walletHandler'
+/** Classes */
+export * as FileHandler from './classes/fileHandler'
+export * as FileIo from './classes/fileIo'
+export * as FolderFileHandler from './classes/folderFileHandler'
+export * as GovHandler from './classes/govHandler'
+export * as WalletHandler from './classes/walletHandler'
 
-export {
-  FileIo,
-  FileHandler,
-  WalletHandler
-}
+/** Interfaces */
+export * as IFileHandler from './interfaces/classes/IFileHandler'
+export * as IFileIo from './interfaces/classes/IFileIo'
+export * as IFolderFileHandler from './interfaces/classes/IFolderFileHandler'
+export * as IGovHandler from './interfaces/classes/IGovHandler'
+export * as IWalletHandler from './interfaces/classes/IWalletHandler'
+
+/** Types */
