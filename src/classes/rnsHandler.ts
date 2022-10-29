f81a77f5cdc78bb3f4faf38ab7c11a6ab57c277d
Erin Rivas <erin@atomicbear.com>
2022-10-29 18:10:47 -0500

    rnsHandler proto rework done

--- src/classes/rnsHandler.ts
+++ src/classes/rnsHandler.ts
@@ -1,36 +1,25 @@
-import { rnsTxClient, rnsQueryApi, rnsQueryClient } from 'jackal.js-protos'
 import { IRnsHandler, IWalletHandler } from '../interfaces/classes'
 import { RnsNames } from 'jackal.js-protos/dist/protos/jackal-dao/canine/jackaldao.canine.rns/module/rest'
 
 export default class RnsHandler implements IRnsHandler {
   private readonly walletRef: IWalletHandler
-  readonly txAddr26657: string
-  readonly queryAddr1317: string
-  private readonly rnsTxClient: any
-  private rnsQueryClient: rnsQueryApi<any>
+  private readonly pH: any
 
-  private constructor (wallet: IWalletHandler, txAddr: string, queryAddr: string, txClient: any, queryClient: rnsQueryApi<any>) {
+  private constructor (wallet: IWalletHandler) {
     this.walletRef = wallet
-    this.txAddr26657 = txAddr
-    this.queryAddr1317 = queryAddr
-    this.rnsTxClient = txClient
-    this.rnsQueryClient = queryClient
+    this.pH = wallet.pH
   }
 
   static async trackRns (wallet: IWalletHandler): Promise<IRnsHandler> {
-    const txAddr = wallet.txAddr26657
-    const queryAddr = wallet.queryAddr1317
-    const txClient = await rnsTxClient(wallet.getSigner(), { addr: txAddr })
-    const queryClient = await rnsQueryClient({ addr: queryAddr })
-    return new RnsHandler(wallet, txAddr, queryAddr, txClient, queryClient)
+    return new RnsHandler(wallet)
   }
 
   async findExistingNames (): Promise<RnsNames[]> {
-    return (await this.rnsQueryClient.queryListOwnedNames(this.walletRef.getJackalAddress())).data.names || []
+    return (await this.pH.rnsQuery.queryListOwnedNames(this.walletRef.getJackalAddress())).data.names || []
   }
   async findMatchingAddress (rns: string): Promise<string> {
     const trueRns = (rns.endsWith('.jkl')) ? rns : `${rns}.jkl`
-    return (await this.rnsQueryClient.queryNames(trueRns)).data.names?.value || ''
+    return (await this.pH.rnsQuery.queryNames(trueRns)).data.names?.value || ''
   }
 }
 
