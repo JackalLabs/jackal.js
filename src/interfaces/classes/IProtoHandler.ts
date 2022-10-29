31e24ea6b08e5e7353ab882a9537fdf867c2cd36
Erin Rivas <erin@atomicbear.com>
2022-10-28 22:03:03 -0500

    typing

--- src/interfaces/classes/IProtoHandler.ts
+++ src/interfaces/classes/IProtoHandler.ts
@@ -1,3 +1,19 @@
 export default interface IProtoHandler {
 
+  broadcaster: any
+  bankQuery: any
+  bankTx: any
+  distributionQuery: any
+  distributionTx: any
+  filetreeQuery: any
+  filetreeTx: any
+  govQuery: any
+  govTx: any
+  rnsQuery: any
+  rnsTx: any
+  stakingQuery: any
+  stakingTx: any
+  storageQuery: any
+  storageTx: any
+
 }
