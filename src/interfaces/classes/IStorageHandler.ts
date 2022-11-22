24fb065b807a471bda80998fd6b016a456129731
Erin Rivas <erin@atomicbear.com>
2022-11-22 15:13:20 -0600

    typo fix

--- src/interfaces/classes/IStorageHandler.ts
+++ src/interfaces/classes/IStorageHandler.ts
@@ -2,7 +2,7 @@ import { DeliverTxResponse } from '@cosmjs/stargate'
 import { IPayBlock, IPayData, IStorageClientUsage } from '@/interfaces'
 
 export default interface IStorageHandler {
-  BuyStorage (forAddress: string, duration: string, bytes: string): Promise<DeliverTxResponse>
+  buyStorage (forAddress: string, duration: string, bytes: string): Promise<DeliverTxResponse>
   getClientUsage (address: string): Promise<IStorageClientUsage>
   getGetPayData (address: string): Promise<IPayData>
   getPayBlocks (blockid: string): Promise<IPayBlock>
