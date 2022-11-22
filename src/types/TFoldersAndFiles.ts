ca544a29a538c76cb35f37315beba7c7bd076cbe
Erin Rivas <erin@atomicbear.com>
2022-11-22 15:34:21 -0600

    type import updates

--- src/types/TFoldersAndFiles.ts
+++ src/types/TFoldersAndFiles.ts
@@ -1,4 +1,4 @@
-import IFileUploadHandler from '../interfaces/classes/IFileUploadHandler'
-import IFolderHandler from '../interfaces/classes/IFolderHandler'
+import IFileUploadHandler from '@/interfaces/classes/IFileUploadHandler'
+import IFolderHandler from '@/interfaces/classes/IFolderHandler'
 
 export type TFileOrFFile = IFileUploadHandler | IFolderHandler
