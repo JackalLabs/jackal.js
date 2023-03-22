5b67eaf1e6538a0e3428cf0984bc187bbf973ae9
Erin Rivas <erin@atomicbear.com>
2023-03-21 22:04:20 -0500

    updated gas figures

--- src/utils/gas.ts
+++ src/utils/gas.ts
@@ -3,12 +3,12 @@ import { IGasHashMap, IGasRate } from '@/interfaces'
 
 const hashMap: IGasHashMap = {
   /** Filetree */
-  '/canine_chain.filetree.MsgPostFile': 142,
+  '/canine_chain.filetree.MsgPostFile': 56,
   '/canine_chain.filetree.MsgAddViewers': 142,
-  '/canine_chain.filetree.MsgPostkey': 142,
-  '/canine_chain.filetree.MsgDeleteFile': 142,
+  '/canine_chain.filetree.MsgPostkey': 12,
+  '/canine_chain.filetree.MsgDeleteFile': 9,
   '/canine_chain.filetree.MsgRemoveViewers': 142,
-  '/canine_chain.filetree.MsgMakeRoot': 142,
+  '/canine_chain.filetree.MsgMakeRoot': 20,
   '/canine_chain.filetree.MsgAddEditors': 142,
   '/canine_chain.filetree.MsgRemoveEditors': 142,
   '/canine_chain.filetree.MsgResetEditors': 142,
@@ -20,25 +20,25 @@ const hashMap: IGasHashMap = {
   /** RNS */
   '/canine_chain.rns.MsgAcceptBid': 142,
   '/canine_chain.rns.MsgAddRecord': 142,
-  '/canine_chain.rns.MsgBid': 142,
-  '/canine_chain.rns.MsgBuy': 142,
+  '/canine_chain.rns.MsgBid': 23,
+  '/canine_chain.rns.MsgBuy': 35,
   '/canine_chain.rns.MsgCancelBid': 142,
   '/canine_chain.rns.MsgDelist': 142,
   '/canine_chain.rns.MsgDelRecord': 142,
-  '/canine_chain.rns.MsgInit': 142,
-  '/canine_chain.rns.MsgList': 142,
-  '/canine_chain.rns.MsgRegister': 142,
+  '/canine_chain.rns.MsgInit': 15,
+  '/canine_chain.rns.MsgList': 14,
+  '/canine_chain.rns.MsgRegister': 37,
   '/canine_chain.rns.MsgTransfer': 142,
   '/canine_chain.rns.MsgUpdate': 142,
   /** Storage */
-  '/canine_chain.storage.MsgPostContract': 142,
+  '/canine_chain.storage.MsgPostContract': 126,
   '/canine_chain.storage.MsgPostproof': 142,
-  '/canine_chain.storage.MsgSignContract': 142,
+  '/canine_chain.storage.MsgSignContract': 82,
   '/canine_chain.storage.MsgSetProviderIP': 142,
   '/canine_chain.storage.MsgSetProviderKeybase': 142,
   '/canine_chain.storage.MsgSetProviderTotalspace': 142,
   '/canine_chain.storage.MsgInitProvider': 142,
-  '/canine_chain.storage.MsgCancelContract': 142,
+  '/canine_chain.storage.MsgCancelContract': 29,
   '/canine_chain.storage.MsgBuyStorage': 142,
   '/canine_chain.storage.MsgClaimStray': 142,
   '/canine_chain.storage.MsgUpgradeStorage': 142,
@@ -62,7 +62,7 @@ const hashMap: IGasHashMap = {
   '/cosmos.staking.v1beta1.MsgEditValidator': 142,
   '/cosmos.staking.v1beta1.MsgUndelegate': 142
 }
-const baseRate = 142
+const baseRate = 56
 
 export function estimateGas (msgArray: EncodeObject[]): number {
   const gas = msgArray.reduce((acc, curr) => {
