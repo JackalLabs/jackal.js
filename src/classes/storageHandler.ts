fd52643cb6752656d33809255a86969e13748221
Erin Rivas <erin@atomicbear.com>
2023-04-24 18:06:45 -0500

    adding sharing tracking

--- src/classes/storageHandler.ts
+++ src/classes/storageHandler.ts
@@ -1,8 +1,13 @@
 import { IProtoHandler, IStorageHandler, IWalletHandler } from '@/interfaces/classes'
 import { EncodeObject } from '@cosmjs/proto-signing'
-import { IPayData, IStoragePaymentInfo } from '@/interfaces'
+import { IEditorsViewers, IFiletreeParsedContents, IPayData, ISharedTracker, IStoragePaymentInfo } from '@/interfaces'
 import { numTo3xTB, numToWholeTB } from '@/utils/misc'
 import { DeliverTxResponse } from '@cosmjs/stargate'
+import { hashAndHex, merkleMeBro } from '@/utils/hash'
+import { aesToString, compressEncryptString, decryptDecompressString, genIv, genKey, stringToAes } from '@/utils/crypt'
+import { MsgMakeRoot } from 'jackal.js-protos/dist/postgen/canine_chain/filetree/tx'
+import { IQueryFileTree } from 'jackal.js-protos'
+import { Files } from 'jackal.js-protos/dist/postgen/canine_chain/filetree/files'
 
 export default class StorageHandler implements IStorageHandler {
   private readonly walletRef: IWalletHandler
@@ -63,4 +68,100 @@ export default class StorageHandler implements IStorageHandler {
     const result = (await this.pH.storageQuery.queryStoragePaymentInfo({ address })).value.storagePaymentInfo
     return (result) ? result : { spaceAvailable: 0, spaceUsed: 0, address: '' }
   }
+
+  /** Manage FT Noti */
+  async saveSharing (toAddress: string, shared: ISharedTracker): Promise<EncodeObject> {
+    const aes = {
+      iv: genIv(),
+      key: await genKey()
+    }
+    const msg: any = {
+      creator: this.walletRef.getJackalAddress(),
+      contents: await compressEncryptString(JSON.stringify(shared), aes.key, aes.iv),
+      rootHashPath: await merkleMeBro(`s/Sharing/${toAddress}`),
+      trackingNumber: self.crypto.randomUUID()
+    }
+    msg.account = await hashAndHex(msg.creator)
+    const basePerms: any = {
+      num: msg.trackingNumber,
+      aes
+    }
+    const selfPubKey = this.walletRef.getPubkey()
+    const destPubKey = this.walletRef.findPubKey(toAddress)
+    const me = await makePermsBlock({ ...basePerms, pubKey: selfPubKey, usr: msg.creator }, this.walletRef)
+    const them = await makePermsBlock({ ...basePerms, pubKey: destPubKey, usr: toAddress }, this.walletRef)
+    msg.editors = JSON.stringify(me)
+    msg.viewers = JSON.stringify({...me, ...them})
+    return makeSharedBlock(msg, this.pH)
+  }
+  async readSharing (owner: string, rawPath: string): Promise<ISharedTracker> {
+    const hexAddress = await merkleMeBro(rawPath)
+    const hexedOwner = await hashAndHex(`o${hexAddress}${await hashAndHex(owner)}`)
+    const result = await this.pH.fileTreeQuery.queryFiles({ address: hexAddress, ownerAddress: hexedOwner })
+    if (!result.success) {
+      throw new Error('Share Data Not Found')
+    } else {
+      try {
+        const { contents, viewingAccess, trackingNumber } = result.value.files as Files
+        const parsedVA = JSON.parse(viewingAccess)
+        const viewName = await hashAndHex(`s${trackingNumber}${this.walletRef.getJackalAddress()}`)
+        const keys = await stringToAes(this.walletRef, parsedVA[viewName])
+        const final = await decryptDecompressString(contents, keys.key, keys.iv)
+        return JSON.parse(final)
+      } catch (err) {
+        throw new Error(`Storage.Handler - readSharing() JSON Parse Failed: ${err}`)
+      }
+    }
+  }
+  async stopSharing (rawPath: string): Promise<EncodeObject> {
+    const creator = this.walletRef.getJackalAddress()
+    return this.pH.fileTreeTx.msgDeleteFile({
+      creator,
+      hashPath: await merkleMeBro(rawPath),
+      account: await hashAndHex(creator),
+    })
+  }
+
+}
+
+async function getFileChainData (hexAddress: string, owner: string, fileTreeQuery: IQueryFileTree) {
+  console.log('getFileChainData')
+  console.log(hexAddress)
+  console.log(owner)
+  const fileResp = await fileTreeQuery.queryFiles({ address: hexAddress, ownerAddress: owner })
+  console.log(fileResp)
+  if (!fileResp.value || !fileResp.value.files) throw new Error('No address found!')
+  const fileData = fileResp.value.files
+  if (!fileResp.success) {
+    fileData.contents = '{ "fids": [] }'
+  }
+  const parsedContents: IFiletreeParsedContents = JSON.parse(fileData.contents)
+  return {
+    version: parsedContents.fids[parsedContents.fids.length - 1],
+    data: fileData
+  }
+}
+
+/** Helpers */
+function makeSharedBlock (msg: MsgMakeRoot, pH: IProtoHandler): EncodeObject {
+  return pH.fileTreeTx.msgMakeRoot({
+    creator: msg.creator,
+    account: msg.account,
+    rootHashPath: msg.rootHashPath,
+    contents: msg.contents,
+    editors: msg.editors,
+    viewers: msg.viewers,
+    trackingNumber: msg.trackingNumber
+  })
+}
+async function makePermsBlock (parts: any, walletRef: IWalletHandler): Promise<{ [user: string]: string }> {
+  const perms: { [user: string]: string } = {}
+  const user = await hashAndHex(`s${parts.num}${parts.usr}`)
+  const value = await aesToString(
+    walletRef,
+    parts.pubKey,
+    parts.aes
+  )
+  perms[user] = value
+  return perms
 }
