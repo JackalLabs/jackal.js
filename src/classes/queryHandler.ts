28b9ab554fa3899a05120c87bfe13f634aa46fa0
Erin Rivas <erin@atomicbear.com>
2023-06-06 12:59:37 -0500

    js docs finished for: queryHandler.ts

--- src/classes/queryHandler.ts
+++ src/classes/queryHandler.ts
@@ -1,5 +1,12 @@
 import { IQueryHandler } from '@/interfaces/classes'
-import { IAllQuery, QueryBuilder } from 'jackal.js-protos'
+import {
+  IAllQuery, IQueryABCI, IQueryBank, IQueryDistribution,
+  IQueryFileTree, IQueryGov,
+  IQueryJklMint,
+  IQueryNotifications,
+  IQueryOracle, IQueryRns, IQueryStaking, IQueryStorage,
+  QueryBuilder
+} from 'jackal.js-protos'
 
 export default class QueryHandler implements IQueryHandler {
   protected readonly allQueryClients: IAllQuery
@@ -16,9 +23,9 @@ export default class QueryHandler implements IQueryHandler {
   }
 
   /**
-   *
-   * @param {string} queryUrl
-   * @returns {Promise<QueryHandler>}
+   * Async wrapper to create a QueryHandler instance.
+   * @param {string} queryUrl - URL to API query node to use for requests.
+   * @returns {Promise<QueryHandler>} - Instance of QueryHandler.
    */
   static async trackQuery(queryUrl?: string) {
     const builder = new QueryBuilder(queryUrl)
@@ -27,39 +34,95 @@ export default class QueryHandler implements IQueryHandler {
   }
 
   /** Custom */
-  get fileTreeQuery() {
+
+  /**
+   * Expose FileTree query client instance.
+   * @returns {IQueryFileTree}
+   */
+  get fileTreeQuery(): IQueryFileTree {
     return this.allQueryClients.fileTree
   }
-  get jklMintQuery() {
+
+  /**
+   * Expose JklMint query client instance.
+   * @returns {IQueryJklMint}
+   */
+  get jklMintQuery(): IQueryJklMint {
     return this.allQueryClients.jklMint
   }
-  get notificationsQuery() {
+
+  /**
+   * Expose Notifications query client instance.
+   * @returns {IQueryNotifications}
+   */
+  get notificationsQuery(): IQueryNotifications {
     return this.allQueryClients.notifications
   }
-  get oracleQuery() {
+
+  /**
+   * Expose Oracle query client instance.
+   * @returns {IQueryOracle}
+   */
+  get oracleQuery(): IQueryOracle {
     return this.allQueryClients.oracle
   }
-  get rnsQuery() {
+
+  /**
+   * Expose Rns query client instance.
+   * @returns {IQueryRns}
+   */
+  get rnsQuery(): IQueryRns {
     return this.allQueryClients.rns
   }
-  get storageQuery() {
+
+  /**
+   * Expose Storage query client instance.
+   * @returns {IQueryStorage}
+   */
+  get storageQuery(): IQueryStorage {
     return this.allQueryClients.storage
   }
 
   /** Static */
-  get ABCIQuery() {
+
+  /**
+   * Expose ABCI query client instance.
+   * @returns {IQueryABCI}
+   * @constructor
+   */
+  get ABCIQuery(): IQueryABCI {
     return this.allQueryClients.abci
   }
-  get bankQuery() {
+
+  /**
+   * Expose Bank query client instance.
+   * @returns {IQueryBank}
+   */
+  get bankQuery(): IQueryBank {
     return this.allQueryClients.bank
   }
-  get distributionQuery() {
+
+  /**
+   * Expose Distribution query client instance.
+   * @returns {IQueryDistribution}
+   */
+  get distributionQuery(): IQueryDistribution {
     return this.allQueryClients.distribution
   }
-  get govQuery() {
+
+  /**
+   * Expose Gov query client instance.
+   * @returns {IQueryGov}
+   */
+  get govQuery(): IQueryGov {
     return this.allQueryClients.gov
   }
-  get stakingQuery() {
+
+  /**
+   * Expose Staking query client instance.
+   * @returns {IQueryStaking}
+   */
+  get stakingQuery(): IQueryStaking {
     return this.allQueryClients.staking
   }
 }
