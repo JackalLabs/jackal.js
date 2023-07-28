2774748fb6958ba66a8cfbb0e0f95c1f203f3472
Erin Rivas <erin@atomicbear.com>
2023-07-28 15:47:00 -0500

    type fixes

--- src/types/TWalletExtensions.ts
+++ src/types/TWalletExtensions.ts
@@ -1,5 +1,5 @@
 import { Keplr } from '@keplr-wallet/types'
 import { Leap } from '@/types/leap'
-import { ICustomWallet } from '@/interfaces/classes'
+import { IMnemonicWallet } from '@/interfaces/classes'
 
-export type TWalletExtensions = Keplr | Leap | ICustomWallet
+export type TWalletExtensions = Keplr | Leap | IMnemonicWallet
